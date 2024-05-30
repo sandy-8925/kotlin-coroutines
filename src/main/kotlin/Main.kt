b5a813eb0b6c5df05f00830a0b66f4cceac56a80
Sandeep Raghuraman <sandy.8925@gmail.com>
2024-05-30 21:57:03 +0530

    Ninth coroutine!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,26 @@ package org.example.coroutines
 import kotlinx.coroutines.*
 
 fun main() {
-    eighthCoroutine()
+    ninthCoroutine()
+}
+
+private fun ninthCoroutine() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val job = launch(Dispatchers.Default) {
+        var nextPrintTime = startTime
+        var i = 0
+        while (isActive) { // computation loop, just wastes CPU
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("job: I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
 }
 
 private fun eighthCoroutine() = runBlocking {
