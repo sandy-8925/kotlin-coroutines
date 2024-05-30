d474d388818a12a18126e3ca16910f3715d15d5c
Sandeep Raghuraman <sandy.8925@gmail.com>
2024-05-30 18:26:16 +0530

    Second coroutines example!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,22 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
+    secondCoroutine()
+}
+
+private fun secondCoroutine() {
+    runBlocking {
+        launch { doWorld() }
+        println("Hello")
+    }
+}
+
+private suspend fun doWorld() {
+    delay(1000L)
+    println("World")
+}
+
+private fun firstCoroutine() {
     runBlocking {
         launch {
             delay(1000L)
